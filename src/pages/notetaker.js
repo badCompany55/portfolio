788fa1cb31300d35ed893e13de5cd96e96ccb818
badCompany55 <zachirvin45@gmail.com>
2019-10-07 07:55:32 -0500

    Remove markdown add base styling for individual app pages

--- src/pages/notetaker.js
+++ src/pages/notetaker.js
@@ -1,30 +1,36 @@
 import React from "react"
 import Layout from "../components/layout.js"
+import "../styles/apps.scss"
 
 const NoteTaker = () => {
   return (
     <Layout>
-      <div className="appCont">
-        <div className="intro">
-          <h2>Note Taking App</h2>
-          <div>A implementation of reminders and notes in a web app</div>
-        </div>
-        <div className="appBody">
-          <p>
-            Keeping a todo list and reminders is a valid way to stay on track
-            with the goals and tasks that need to be completed during the day.
-            This app offers a way to do this and is accessible anywhere the one
-            has an internet connection.
-          </p>
-        </div>
-        <div className="content">
-          <div>
-            <h2>Front End / UI</h2>
-            <p>
-              The Front end is build out using react. The user can sort notes,
-              search notes, and drag and drop notes into a different order. The
-              app also integrates a word processor for formatting capabilities.
+      <div className="main">
+        <div className="appCont">
+          <div className="intro">
+            <h2 className="mainHeading">Note Taking App</h2>
+            <div className="subHeading">
+              A implementation of reminders and notes in a web app
+            </div>
+          </div>
+          <div className="appBody">
+            <p className="text notHidden">
+              Keeping a todo list and reminders is a valid way to stay on track
+              with the goals and tasks that need to be completed during the day.
+              This app offers a way to do this and is accessible anywhere the
+              one has an internet connection.
             </p>
+          </div>
+          <div className="content">
+            <div>
+              <h2 className="subHeading">Front End / UI</h2>
+              <p className="text">
+                The Front end is build out using react. The user can sort notes,
+                search notes, and drag and drop notes into a different order.
+                The app also integrates a word processor for formatting
+                capabilities.
+              </p>
+            </div>
           </div>
         </div>
       </div>
--- src/pages/pt3scheduler.js
+++ src/pages/pt3scheduler.js
@@ -1,63 +1,69 @@
 import React from "react"
 import Layout from "../components/layout.js"
+import "../styles/apps.scss"
 
 const Pt3Scheduler = () => {
   return (
     <Layout>
-      <div className="appCont">
-        <div class="intro">
-          <h2>A scheduler app for the busy contractor and client</h2>
-          <div>
-            Simplifies the scheduling process between client and the Contractor
-            to improve efficiency and satisfaction
+      <div className="main">
+        <div className="appCont">
+          <div class="intro">
+            <h2 className="mainHeading">
+              A scheduler app for the busy contractor and client
+            </h2>
+            <div className="subHeading">
+              Simplifies the scheduling process between client and the
+              Contractor to improve efficiency and satisfaction
+            </div>
           </div>
-        </div>
-        <div class="appBody">
-          <p>
-            A group of developers were given the task to create a application
-            that would allow for scheduling of appointments between a contractor
-            and client completly online. As with many things in the busy
-            lifestyle that people lead, one doesn't always have time for a phone
-            conversation just for setting an appointment. This app seeks to
-            remedy this situation and provide an intuitive interface for both
-            parties in the transaction in regards to scheduling and
-            appointments.
-          </p>
-        </div>
-        <div className="content">
-          <div>
-            <h2>Backend and Database</h2>
-            <p>
-              The backend of this project was constructed in Nodejs. It utilizes
-              google oAuth for the sign up and login for easier convenience. If
-              the user doesn't have a google account, or if they want to use a
-              standard login method, they can opt for the typical username and
-              password signup. The authentication is conducted via a JWT token
-              when the user signs up and logs in using this method.
+          <div class="appBody">
+            <p className="text notHidden">
+              A group of developers were given the task to create a application
+              that would allow for scheduling of appointments between a
+              contractor and client completly online. As with many things in the
+              busy lifestyle that people lead, one doesn't always have time for
+              a phone conversation just for setting an appointment. This app
+              seeks to remedy this situation and provide an intuitive interface
+              for both parties in the transaction in regards to scheduling and
+              appointments.
             </p>
+          </div>
+          <div className="content">
+            <div>
+              <h2 className="subHeading">Backend and Database</h2>
+              <p className="text">
+                The backend of this project was constructed in Nodejs. It
+                utilizes google oAuth for the sign up and login for easier
+                convenience. If the user doesn't have a google account, or if
+                they want to use a standard login method, they can opt for the
+                typical username and password signup. The authentication is
+                conducted via a JWT token when the user signs up and logs in
+                using this method.
+              </p>
 
-            <p>
-              For the database, the app uses PostgreSQL. The schema is set up
-              with tables for Contactors, Schedules, Users, Services,
-              Appointments, and Feedback allowing for quick querying of the data
-              for each particular use case. For a more in depth read of the
-              particulars feel free to check out the github repo for the
-              project.
-            </p>
-          </div>
-          <div>
-            <h2>Front End / UI</h2>
-            <p>
-              The Front end is build entirely in React. The calendar / scheduler
-              is built using the date-fns library. It provides helper methods
-              for date objects that make creating a calendar from scratch more
-              intuitive.
-            </p>
-            <p>
-              For animation purposes, such as dropdowns for the calendar, page
-              transitions, etc, the GSAP library was used as well a fair amount
-              of standard css animations.
-            </p>
+              <p className="text">
+                For the database, the app uses PostgreSQL. The schema is set up
+                with tables for Contactors, Schedules, Users, Services,
+                Appointments, and Feedback allowing for quick querying of the
+                data for each particular use case. For a more in depth read of
+                the particulars feel free to check out the github repo for the
+                project.
+              </p>
+            </div>
+            <div>
+              <h2 className="subHeading">Front End / UI</h2>
+              <p className="text">
+                The Front end is build entirely in React. The calendar /
+                scheduler is built using the date-fns library. It provides
+                helper methods for date objects that make creating a calendar
+                from scratch more intuitive.
+              </p>
+              <p className="text">
+                For animation purposes, such as dropdowns for the calendar, page
+                transitions, etc, the GSAP library was used as well a fair
+                amount of standard css animations.
+              </p>
+            </div>
           </div>
         </div>
       </div>
